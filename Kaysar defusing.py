aef2aff798491b939bb503c99006bbbfe2945800
erpan <1499001215@qq.com>
2020-09-26 16:16:08 +0800

    kaysar

---
+++ Kaysar defusing.py
@@ -0,0 +1,10 @@
+print("欢迎来到Kaysar解密系统")
+kirguz=input("请输入一串字符串:")
+surguz = int(input("偏移值"))
+for c in kirguz:
+      if "a"<=c<="z":
+            print(chr(ord("a") + (ord(c) - ord("a") - surguz) %26), end= "")
+      elif "A"<=c<="Z":
+            print(chr(ord("A") +(ord(c) - ord("A") - surguz) %26), end= "")
+      else:
+            print(c, end= "")
